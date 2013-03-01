5084a6a26b1cb00924121b6d60d9977a125ab6a6
Pascal Reinhardt <pasrein@web.de>
2013-03-01 18:01:04 +0100

    Enhanced system.py PythonPlot
    Wrapped plot in a function to call and added functionality to pass file and benchmark name as parameters

--- benchmarks/Stride.py
+++ benchmarks/Stride.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# customizable settings for python plot
+# customizable python plot settings for Stride benchmark
 
 #Specify the title of your Plot (default: name of your benchmark)
 settings['title'] = 'Stride'
@@ -8,11 +8,17 @@ settings['title'] = 'Stride'
 settings['xLabel'] = 'Stride in Bytes'
 #Uncomment next line to specify a label for the y axis (default: list of plotted column names)
 settings['yLabel'] = 'CPU Cycles per Element'
+
+#Uncomment next line if the yAxis tics should be divided by a special divider (like number on lines)
+settings['yDivider'] = 4096
  
 #Uncomment next line to specify the scale of your benchmark (default: linear)
 settings['xScale'] = 'log'
 #Uncomment next line to specify the base of a logarithmic scale (dafault: 10) 
 settings['xScaleBase'] = 2
+
+#Uncomment next line get a grid to the plot (default none, possible: xAxis, yAxis, both, none)
+settings['grid'] = 'yAxis'
 
 #Specify the columns you want to have in your plot
 settings['plotList'] = ['random_PAPI_TOT_CYC_y', 'sequential_forwards_PAPI_TOT_CYC_y']
--- benchmarks/system.py
+++ benchmarks/system.py
@@ -5,54 +5,88 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import csv
 import string
+import sys
+from optparse import OptionParser
 
-csvReader = csv.reader(open('../results/Stride/Stride_1.result.csv'))
+def getOptionParser():
+    parser = OptionParser()
+    parser.add_option('-f', '--file', help="set csv file", type='string', dest='file')
+    parser.add_option('-n', '--name', help="set benchmark name", type='string', dest='name')
+    return parser
 
-#defining default settings (in extra file too?)
-settings = {}
-settings['title'] = 'Stride'
-settings['xScale'] = 'linear'
-settings['xScaleBase'] = 10
-settings['xLabel'] = 'par_stride'
-settings['yLabel'] = 'CPU Cycles'
-settings['plotList'] = []
+def plot(csvFile, benchmarkName):
 
-#executing the particular script so settings is filled with customization
-execfile('Stride.py')	
+	csvReader = csv.reader(open(csvFile))
 
-line = csvReader.next()
-lineContents = line[0].split(' ')
-lineIndices = list()
-for entry in lineContents:
-	for counter in settings['plotList']:
-		if entry == counter:
-			lineIndices.append(lineContents.index(entry))
+	#defining default settings (in extra file too?)
+	settings = {}
+	settings['title'] = 'Stride'
+	settings['xScale'] = 'linear'
+	settings['xScaleBase'] = 10
+	settings['xLabel'] = 'par_stride'
+	settings['yLabel'] = 'CPU Cycles'
+	settings['yDivider'] = 1
+	settings['xDivider'] = 1
+	settings['grid'] = 'none'
+	settings['plotList'] = []
 
-x = list()
-y = list()
-for index in lineIndices:
-	z = list()
-	y.append(z)
+	#executing the particular script so settings is filled with customization
+	# execfile(options.name + '.py')	
+	execfile(benchmarkName + '.py')
 
-for line in csvReader:
+	line = csvReader.next()
 	lineContents = line[0].split(' ')
-	x.append(int(lineContents[0]))
+	lineIndices = list()
+	for entry in lineContents:
+		for counter in settings['plotList']:
+			if entry == counter:
+				lineIndices.append(lineContents.index(entry))
+
+	x = list()
+	y = list()
+	for index in lineIndices:
+		z = list()
+		y.append(z)
+
+	for line in csvReader:
+		lineContents = line[0].split(' ')
+		x.append(int(lineContents[0]) / settings['xDivider'])
+		for i in range(0,len(lineIndices)):
+			y[i].append(int(lineContents[lineIndices[i]]) / settings['yDivider'])
+
+
+	yy = np.arange(24)
+	yy.shape = (6,4)
+
+	mpl.rc('lines', linewidth=2)
+
+	fig = plt.figure()
+	mpl.rcParams['axes.color_cycle'] = ['r', 'g', 'b', 'c']
+	ax = fig.add_subplot(2,1,1)
 	for i in range(0,len(lineIndices)):
-		y[i].append(int(lineContents[lineIndices[i]]))
+		ax.plot(x, y[i])
+	ax.set_title(settings['title'])
+	ax.set_xlabel(settings['xLabel'])
+	ax.set_ylabel(settings['yLabel'])
+	ax.set_xscale(settings['xScale'], basex=settings['xScaleBase'])
+	if settings['grid'] is 'yAxis':
+		ax.yaxis.grid(True)
 
-yy = np.arange(24)
-yy.shape = (6,4)
+	ax.annotate('Cache Linesize', (64, 500), xytext=None, xycoords='data', textcoords='data', arrowprops=None)
 
-mpl.rc('lines', linewidth=2)
+	plotDir = csvFile[:csvFile.rfind('/') + 1]
+	plotName = csvFile[csvFile.rfind('/'):]
+	plotName = plotName[:plotName.find('.')]
+	
+	plt.savefig(plotDir + plotName + '.pdf')
+	plt.show()
 
-fig = plt.figure()
-mpl.rcParams['axes.color_cycle'] = ['r', 'g', 'b', 'c']
-ax = fig.add_subplot(2,1,1)
-for i in range(0,len(lineIndices)):
-	ax.plot(x, y[i])
-ax.set_title(settings['title'])
-ax.set_xlabel(settings['xLabel'])
-ax.set_ylabel(settings['yLabel'])
-ax.set_xscale(settings['xScale'], basex=settings['xScaleBase'])
 
-plt.show()
+parser = getOptionParser()
+(options, args) = parser.parse_args(sys.argv, None)
+
+if options.file == None:
+	parser.print_help()
+	sys.exit(0)
+
+plot(options.file, options.name)
